97a852dab7b6a0ad7f1bd23be62ca543ec53bf97
Paul Klemm <paul.klemm@sf.mpg.de>
2020-04-16 13:25:55 +0200

    Loose magrittr and readr dependency #2

--- R/main.r
+++ R/main.r
@@ -1,18 +1,15 @@
 #' Open data with visidata
 #'
 #' @export
-#' @import magrittr readr
 #' @param dat dataframe; Data you want to to visualize with visidata
 vd <- function(dat) {
   # R gives us a temporary file
   temp_file <- tempfile("dat", fileext = ".csv")
   # Write input to the temporary file
-  dat %>%
-    readr::write_csv(temp_file)
+  write.csv(dat, temp_file)
   # Invoke visidata
   system(paste0("vd ", temp_file))
   # Delete the temporary file
-  file.remove(temp_file) %>%
-    # We wrap it in "invisible" to remove the "TRUE" output when file.remove deleted the temporary file
-    invisible()
+  # We wrap it in "invisible" to remove the "TRUE" output when file.remove deleted the temporary file
+  invisible(file.remove(temp_file))
 }
