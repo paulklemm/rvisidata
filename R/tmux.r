21e2f8a2422abe060c81dcd81d086ffaf625a8a0
Paul Klemm <paul.klemm@sf.mpg.de>
2020-04-17 15:31:17 +0200

    Clean up tmux.r

--- R/tmux.r
+++ R/tmux.r
@@ -1,17 +1,21 @@
-
+#' Get visidata command depedning on the TMUX settings
+#' @return visidata command as string
 get_vd_cmd <- function() {
   tmux_cmd <- getOption("rvisidata.tmux")
-  
-  if (Sys.getenv("TMUX", unset = "TMUX_IS_UNSET") == "TMUX_IS_UNSET") return("vd")
-  
+
+  if (Sys.getenv("TMUX", unset = "TMUX_IS_UNSET") == "TMUX_IS_UNSET")
+    return("vd")
+
   # Check for null before checking value
   # Null input clobbers logical returns (returns 0 length)
   # Default to having tmux enabled
-  if (is.null(tmux_cmd)) return(system.file("tmux_pane.sh", package = "rvisidata"))
-  
-  # If explicitly set to false, diable tmux
-  if (tmux_cmd == FALSE ) return("vd")
-  
+  if (is.null(tmux_cmd))
+    return(system.file("tmux_pane.sh", package = "rvisidata"))
+
+  # If explicitly set to false, disable tmux
+  if (tmux_cmd == FALSE)
+    return("vd")
+
   # Users can choose their own script
   return(tmux_cmd)
 }
