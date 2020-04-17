c9dac01847b6965652bc847a3dd5ec9362ec542b
Paul Klemm <paul.klemm@sf.mpg.de>
2020-04-17 14:46:47 +0200

    Cleanup main.r

--- R/main.r
+++ R/main.r
@@ -5,8 +5,8 @@
 vd <- function(dat) {
   file_ext <- ".csv"
   serialize_fun <- write.csv
-  
-  # Prefer to use json over the sadly ubiquitous, wack CSV format
+
+  # Prefer to use json over CSV format
   if (requireNamespace("jsonlite", quietly = TRUE)) {
     file_ext <- ".json"
     serialize_fun <- jsonlite::write_json
@@ -16,17 +16,15 @@ vd <- function(dat) {
   temp_file <- tempfile("dat", fileext = file_ext)
   # Write input to the temporary file
   serialize_fun(dat, temp_file)
-  
-  # Check if we should use vd directly, 
-  # or a tmux wrapper
-  vd_cmd <- get_vd_cmd() 
+
+  # Check if we should use vd directly or a tmux wrapper
+  vd_cmd <- get_vd_cmd()
 
   # Invoke visidata
   system2(vd_cmd, args = temp_file)
-  # We do not delete the temporary file, as if tmux is used, 
+  # We do not delete the temporary file, as if tmux is used,
   # vd will run asynchronously, and we can't guarantee that it's opened
-  # the file yet. 
+  # the file yet.
   # We can rely on R to clean it up when the session ends
-
   invisible(dat)
 }
