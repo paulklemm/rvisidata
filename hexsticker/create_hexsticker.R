b681c5859b9acf7db79ac14e1bd5716b17e6329e
Paul Klemm <paul.klemm@posteo.net>
2024-05-08 09:11:25 +0000

    Add hexsticker

---
+++ hexsticker/create_hexsticker.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+
+sticker(
+  "hexsticker/rvisidata_logo.png",
+  package="",
+  s_x=1,
+  s_y=1,
+  s_width=.8,
+  s_height=2,
+  h_fill="#121723",
+  # h_color="#A26E47",
+  h_color="#15394B",
+  filename="hexsticker/rvisidata_hexsticker.png",
+  l_x = 1
+)
